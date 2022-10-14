7e49d3301a9a933b26fd24ef8b072b8e5da40688
Ismael De Souza Dias <ismaelnamira.i.s.d@gmail.com>
2022-10-14 18:54:14 -0300

    Mario Jump

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,7 @@
 const mario = document.querySelector('.mario')
 const cano = document.querySelector('.cano')
+const perdeu = document.querySelector(".perdeu")
+const nuvens = document.querySelector('.nuvens')
 
 const pular = () => {
     mario.classList.add('pular')
@@ -15,18 +17,24 @@ const loop = setInterval(() => {
 
     const canoPosicao = cano.offsetLeft
     const marioPosicao = +window.getComputedStyle(mario).bottom.replace('px', '')
+    const nuvensPosicao = nuvens.offsetLeft
 
     if (canoPosicao <= 120 && canoPosicao > 0 && marioPosicao <110) {
 
-        cano.style.animation = 'nome'
+        cano.style.animation = 'none'
         cano.style.left = `${canoPosicao}px`
 
-        mario.style.animation = 'nome'
+        mario.style.animation = 'none'
         mario.style.bottom = `${marioPosicao}px`
 
         mario.src = './imagens/game-over.png'
         mario.style.width = '75px'
         mario.style.marginLeft = '50px'
+
+        nuvens.style.animation = 'none'
+        nuvens.style.left = `${nuvensPosicao}px`
+
+        perdeu.textContent = 'Game Over'
 
         clearInterval(loop)
 
